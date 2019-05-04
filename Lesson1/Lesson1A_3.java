1499ada4d328c3b52a7ab78745dfc011cddc9b08
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-05-05 01:22:05 +0300

    Lesson1A_3

---
+++ Lesson1/Lesson1A_3.java
@@ -0,0 +1,19 @@
+package Lesson1;
+
+import java.util.Random;
+
+public class Lesson1A_3 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(random.nextInt(100));
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print(random.nextInt(100) + " ");
+        }
+    }
+}
