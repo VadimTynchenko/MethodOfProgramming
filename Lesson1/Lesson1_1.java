2513b5c63e194eaaf93427fe41a3ef7c130ddf1e
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-05-05 00:35:18 +0300

    Lesson1A_1

---
+++ Lesson1/Lesson1_1.java
@@ -0,0 +1,13 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class Lesson1_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+
+        System.out.println("Hello, " + name);
+    }
+}
