efcc412577dfcafe30c00bc074c3504de071785b
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-05-05 01:18:16 +0300

    Lesson1A_2

--- Lesson1/Lesson1A_2.java
+++ Lesson1/Lesson1A_2.java
@@ -1,4 +1,18 @@
 package Lesson1;
 
+import java.util.Scanner;
+
 public class Lesson1A_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] x = new int[5];
+
+        for (int i = 0; i < x.length; i++) {
+            x[i] = sc.nextInt();
+        }
+
+        for (int i = x.length - 1; i >= 0; i--) {
+            System.out.println(x[i]);
+        }
+    }
 }
