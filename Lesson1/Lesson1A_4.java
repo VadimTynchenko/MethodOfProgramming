b4c222ef9357c53452d6adf8c99903bbb3752206
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-05-05 01:28:57 +0300

    Lesson1A_4

---
+++ Lesson1/Lesson1A_4.java
@@ -0,0 +1,22 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class Lesson1A_4 {
+    public static void main(String[] args) {
+        String password = "12345";
+
+        equality(password);
+    }
+
+    private static void equality(String pass) {
+        Scanner scan = new Scanner(System.in);
+
+        if (scan.nextLine().equals(pass)) {
+            System.out.println("Correct");
+        } else {
+            System.out.println("Wrong");
+            equality(pass);
+        }
+    }
+}
