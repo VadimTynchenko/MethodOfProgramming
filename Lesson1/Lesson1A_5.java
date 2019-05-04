1f6f67d4930fa052b646dd820195c98a3b54d345
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-05-05 01:32:52 +0300

    Lesson1A_5

---
+++ Lesson1/Lesson1A_5.java
@@ -0,0 +1,22 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class Lesson1A_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        int res = 0;
+        int[] nums = new int[count];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        for (int num : nums) {
+            res += num;
+        }
+
+        System.out.println(res);
+    }
+}
