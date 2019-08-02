e596aa186110e325faaca2d8b433a9649abd7d84
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-08-02 19:56:14 +0300

    Refactor for launching from Terminal

--- Lesson1/A/Lesson1A_2.java
+++ Lesson1/A/Lesson1A_2.java
@@ -1,18 +1,9 @@
 package Lesson1.A;
 
-import java.util.Scanner;
-
 public class Lesson1A_2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] x = new int[5];
-
-        for (int i = 0; i < x.length; i++) {
-            x[i] = sc.nextInt();
-        }
-
-        for (int i = x.length - 1; i >= 0; i--) {
-            System.out.println(x[i]);
+        for (int i = args.length - 1; i >= 0; i--) {
+            System.out.print(args[i] + " ");
         }
     }
 }
