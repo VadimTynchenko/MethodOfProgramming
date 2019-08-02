15da68f1c34bc8531c3f7ae98755cffcce252293
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-08-02 20:04:06 +0300

    Refactor for launching from Terminal

--- Lesson1/A/Lesson1A_4.java
+++ Lesson1/A/Lesson1A_4.java
@@ -1,22 +1,11 @@
 package Lesson1.A;
 
-import java.util.Scanner;
-
 public class Lesson1A_4 {
     public static void main(String[] args) {
-        String password = "12345";
+        String password = "password";
 
-        equality(password);
-    }
-
-    private static void equality(String pass) {
-        Scanner scan = new Scanner(System.in);
-
-        if (scan.nextLine().equals(pass)) {
-            System.out.println("Correct");
-        } else {
-            System.out.println("Wrong");
-            equality(pass);
-        }
+        if (password.equals(args[0])) {
+            System.out.println(true);
+        } else System.out.println(false);
     }
 }
