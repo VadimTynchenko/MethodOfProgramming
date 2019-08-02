05c281b17ab0c841450aa9a32ee6c55c34d989ff
Vadim Tynchenko <vadim.tynchenko@icloud.com>
2019-08-02 20:01:38 +0300

    Refactor for launching from Terminal

--- Lesson1/A/Lesson1A_3.java
+++ Lesson1/A/Lesson1A_3.java
@@ -6,13 +6,13 @@ public class Lesson1A_3 {
     public static void main(String[] args) {
         Random random = new Random();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < Integer.parseInt(args[0]); i++) {
             System.out.println(random.nextInt(100));
         }
 
         System.out.println();
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < Integer.parseInt(args[0]); i++) {
             System.out.print(random.nextInt(100) + " ");
         }
     }
